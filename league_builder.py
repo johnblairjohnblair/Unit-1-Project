86ed712b593335b17e6087e970eb241028a565fb
John Blair <36686406+johnblairjohnblair@users.noreply.github.com>
2018-09-08 15:58:12 -0500

    Added notes

--- league_builder.py
+++ league_builder.py
@@ -1,1 +1,21 @@
 print("League Builder")
+
+#Open csv and read values into dicts
+
+
+#Sort players into dicts based on experience
+
+
+#Initialize team dicts
+
+
+
+#Assign experienced list based on list fill
+
+
+#Assign inexperienced list based on list fill
+
+
+#Print teams and players
+
+
