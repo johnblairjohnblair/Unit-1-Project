59b40efa7df50043961aa22d1fb81f523962e58b
John Blair <36686406+johnblairjohnblair@users.noreply.github.com>
2018-09-09 00:10:29 -0500

    Program to read file of soccer players and divide into evenly distributed teams based on experience.

--- league_builder.py
+++ league_builder.py
@@ -31,10 +31,10 @@ if __name__ == "__main__":
     for player in players:
         if players[player]['experience'] == 'YES':
             experienced_players[player] = players[player]
-            print("experienced")
+            print(player + "experienced")
         elif players[player]['experience'] == 'NO':
             inexperienced_players[player] = players[player]
-            print("inexp")
+            print(player + "inexp")
         else:
             print("no exp variable")
 
@@ -49,43 +49,82 @@ if __name__ == "__main__":
 
 # Assign experienced list based on list fill
     for player in experienced_players:
-        if len(sharks) == len(dragons) and len(sharks) == len(raptors):
+        lowest_team = min(len(sharks), len(dragons), len(raptors))
+        if len(sharks) == lowest_team:
             sharks[player] = players[player]
-        if len(sharks) <= len(dragons) and len(sharks) <= len(dragons):
-            sharks[player] = players[player]
-        elif len(dragons) <= len(sharks) and len(dragons) <= len(raptors):
+            print(players[player]['name'] + "assigned to sharks")
+        elif len(dragons) == lowest_team:
             dragons[player] = players[player]
-        elif len(raptors) <= len(dragons) and len(raptors) <= len(sharks):
+            print(players[player]['name'] + "assigned to dragons")
+        elif len(raptors) == lowest_team:
             raptors[player] = players[player]
+            print(players[player]['name'] + "assigned to raptors")
         else:
             print("something happened ", player, " not assigned to a team!")
 
 
 #Assign inexperiences list based on least fill
     for player in inexperienced_players:
-        if len(sharks) == len(dragons) and len(sharks) == len(raptors):
+        lowest_team = min(len(sharks), len(dragons), len(raptors))
+        if len(sharks) == lowest_team:
             sharks[player] = players[player]
-        if len(sharks) <= len(dragons) and len(sharks) <= len(dragons):
-            sharks[player] = players[player]
-        elif len(dragons) <= len(sharks) and len(dragons) <= len(raptors):
+            print(players[player]['name'] + "assigned to sharks")
+        elif len(dragons) == lowest_team:
             dragons[player] = players[player]
-        elif len(raptors) <= len(dragons) and len(raptors) <= len(sharks):
+            print(players[player]['name'] + "assigned to dragons")
+        elif len(raptors) == lowest_team:
             raptors[player] = players[player]
+            print(players[player]['name'] + "assigned to raptors")
         else:
             print("something happened ", player, " not assigned to a team!")
 
 # SELF CHECK FOR TEAMS
-    print("Sharks:")
+    print("\nSharks:")
     for player in sharks:
         print(player)
-    print("Dragons:")
+    print("\nDragons:")
     for player in dragons:
         print(player)
-    print("Raptors:")
+    print("\nRaptors:")
     for player in raptors:
         print(player)
 
+
+#Create text to print into text doc
+    def player_info(player):
+        text = player['name'] + ", " + player['experience'] + ", " + player['guardians']
+        return(text)
+
+    def full_message():
+        print("\nSharks:")
+        for player in sharks:
+            player_info(sharks[player])
+
+        print("\nDragons:")
+        for player in dragons:
+            player_info(dragons[player])
+
+        print("\nRaptors:")
+        for player in raptors:
+            player_info(raptors[player])
+
+        return("This is what returned from full_message function...")
+
 # Write teams and players to doc
+    final = open("teams.txt", "w")
+    final.write("\nSharks:")
+    for player in sharks:
+        final.write("\n" + player_info(sharks[player]))
+    final.write("\n\nDragons:")
+    for player in dragons:
+        final.write("\n" + player_info(dragons[player]))
+    final.write("\n\nRaptors:")
+    for player in raptors:
+        final.write("\n" + player_info(raptors[player]))
+
+
+    final.close()
+
 
 
 # Generate welcome letters
